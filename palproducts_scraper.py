3cbd54d26d7e7ea3923bc657bb8890108ffc32d3
Janit Sriganeshaelankovan <janits_27@hotmail.com>
2019-01-17 12:49:34 -0500

    fixed palproducts_scraper.py

--- palproducts_scraper.py
+++ palproducts_scraper.py
@@ -23,63 +23,68 @@ from chromedriverpath import CHROMEDRIVER_PATH
 os.chdir(r'C:\Users\janit\Documents\GIT\pal_scraper')
 
 
-# add chrome driver path 
+# ADD CHROME DRIVER'S PATH  
 chrome_driver = CHROMEDRIVER_PATH
 
 
-pa_products = set()
-len(pa_products)
-loop_times = []
-
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0'}
-main_url = r'https://www.princessauto.com/en/search' 
-urls = []
-for x in range (0, 25400, 100):
-    urls.append(main_url + '?Nrpp=100&No={}'.format(str(x)))
-
-print(len(urls))
-
-options = Options()
-prefs = {"profile.managed_default_content_settings.images": 2}
-options.add_experimental_option("prefs", prefs)
-
-driver = webdriver.Chrome(chrome_driver, chrome_options=options)
-driver.maximize_window()
+# WEB SCRAPE PRODUCTS 
+def pal_scraper():
+    pa_products = set()
+    loop_times = []
     
-for idx, u in enumerate(urls):
-    start = time.time()
-    print('On URL {0}: {1}'.format(str(idx), u))
+    main_url = r'https://www.princessauto.com/en/search' 
+    urls = []
+    for x in range (0, 25400, 100):
+        urls.append(main_url + '?Nrpp=100&No={}'.format(str(x)))
     
-    driver.get(u)
-    source = driver.page_source 
-
-    page_soup = soup(source, 'lxml')
-    body = page_soup.body
+    print(len(urls))
     
-    products = page_soup.find_all('a', {"class":'cta-magnify quickview-modal'})
+    options = Options()
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    options.add_experimental_option("prefs", prefs)
     
-    if products:
-        print(len(products))
-        for product in products:
-            product_name = product['data-product-name']
-            product_SKU = product['data-product-id']
-            product_category = product['data-category-trail']
-            pa_products.add((product_SKU, product_name, product_category))
-    print('The length of the Product List is {}'.format(len(pa_products)))
-    loop_times.append(time.time() - start)
-    print('This loop took {}'.format(str(time.time() - start)))
+    driver = webdriver.Chrome(chrome_driver, chrome_options=options)
+    driver.maximize_window()
+        
+    for idx, u in enumerate(urls):
+        start = time.time()
+        print('On URL {0}: {1}'.format(str(idx), u))
+        
+        driver.get(u)
+        source = driver.page_source 
+    
+        page_soup = soup(source, 'lxml')
+        
+        products = page_soup.find_all('a', {"class":'cta-magnify quickview-modal'})
+        
+        if products:
+            print(len(products))
+            for product in products:
+                product_name = product['data-product-name']
+                product_SKU = product['data-product-id']
+                product_category = product['data-category-trail']
+                pa_products.add((product_SKU, product_name, product_category))
+        print('The length of the Product List is {}'.format(len(pa_products)))
+        loop_times.append(time.time() - start)
+        print('This loop took {}'.format(str(time.time() - start)))
+
+    return pa_products
 
 
+pal_products = pal_scraper()
 
+
+# WRITE TO TXT FORMAT
 with open('PA_PRODUCTS_ALL_{}.txt'.format(datetime.datetime.now().strftime('%d_%m_%Y')), 'a') as f:
-    for x in pa_products:
+    for x in pal_products:
         xx = ','.join(x)
         f.write(xx +'\n')
 
 
-with open('PA_PRODUCTS_ALL_12_22_2018.csv', 'w', newline='') as f:
+# WRITE TO CSV FORMAT
+with open('PA_PRODUCTS_ALL_{}.csv'.format(datetime.datetime.now().strftime('%d_%m_%Y')), 'w', newline='') as f:
     header = ['SKU', 'PRODUCT NAME', 'PRODUCT CATEGORY']
     writer = csv.DictWriter(f, fieldnames=header)
     writer.writeheader()
-    for p in pa_products:
+    for p in pal_products:
         writer.writerow({'SKU':p[0], 'PRODUCT NAME':p[1], 'PRODUCT CATEGORY':p[2]})
